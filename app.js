1d85a2e6e060f2075a37ea437aff5688779f667f
Ylenia <yleniamighela@gmail.com>
2025-02-06 16:14:01 +0100

    added app.js and changed route row 7

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 // console.log("ciao");
+const express = require('express')
+const app = express()
+const port = 3000
+
+// salvo i file delle rotte in una costante per importarli
+const postsRouter = require('./routers/postsRouter');
+
+// definisco la cartella per i file statici
+app.use(express.static('public'));
+
+
+// definsco la prima rotta con un semplice messaggio
+app.get('/', (req, res) => {
+
+    res.send("Server del mio blog")
+})
+
+
+
+//  invoco la rotta
+app.use("/posts", postsRouter)
+
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+    });
